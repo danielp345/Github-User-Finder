582d1aa8300f646e921d50856918d51620277ce7
danielp345 <danielp345@gmail.com>
2022-02-20 13:19:37 +0100

    fix typo

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -6,12 +6,11 @@ import GithubContext from "../../context/github/GithubContext"
 function UserResults() {
 	const { users, loading } = useContext(GithubContext)
 
-
 	if (!loading) {
 		return (
 			<div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 				{users.map((user) => (
-					<UserItem key={users.id} user={user} />
+					<UserItem key={user.id} user={user} />
 				))}
 			</div>
 		)
